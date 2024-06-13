3593be8229921546945b264ae1c4250531f05a7f
josia.schweizer <josia.schweizer@gmail.com>
2024-06-13 15:59:50 +0200

    added decryption class

---
+++ src/main/java/ch/abacus/SymmetricDecryption.java
@@ -0,0 +1,19 @@
+/*
+ * SymmetricDecryption.java
+ *
+ * Creator:
+ * 13.06.2024 15:59 josia.schweizer
+ *
+ * Maintainer:
+ * 13.06.2024 15:59 josia.schweizer
+ *
+ * Last Modification:
+ * $Id:$
+ *
+ * Copyright (c) 2024 ABACUS Research AG, All Rights Reserved
+ */
+package ch.abacus;
+
+public class SymmetricDecryption {
+
+}
